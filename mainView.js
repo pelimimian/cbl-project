72c0ebcb296c7ddf1b76821957e17063e7da8ef9
pelimimian <pelimimian7@gmail.com>
2020-04-11 23:06:49 -0400

    Added a for loop and created an array of url links

--- mainView.js
+++ mainView.js
@@ -1,3 +1,7 @@
+//API Flickr Info
+//Key caa57ad8874db3c27dd35f3a812b70f4
+//Secret ae4f050c5fdf3b28
+
 function assignPicture(){
     var breakerValue = document.getElementById("17A").id;
    // var pictureDisplayed = false;
@@ -10,21 +14,16 @@ function assignPicture(){
 }
 
 function displayPicture(breakerValue) {
-    var src, src2, src3, src4, src5, src6  = "";
     var compareResult = breakerValue.localeCompare("17A");
         if (compareResult == 0 ){
-        src = "https://live.staticflickr.com/65535/49667651786_eaf87c6e0b_b.jpg";
-        src2 = "https://live.staticflickr.com/65535/49667651751_60cf38e2ae.jpg";
-        src3 = "https://live.staticflickr.com/65535/49667651776_44c46ac95e_b.jpg";
-        src4 = "https://live.staticflickr.com/65535/49667935067_7dbc48bd46_b.jpg";
-        src5 = "https://live.staticflickr.com/65535/49667935052_0a0c888258_b.jpg";
-        src6 = "https://live.staticflickr.com/65535/49667935092_cc4f2aa137_b.jpg";
-        show_image(src, 400,400, "Breaker Value");
-        show_image(src2, 400,400, "Breaker Value");
-        show_image(src3, 400,400, "Breaker Value");
-        show_image(src4, 400,400, "Breaker Value");
-        show_image(src5, 400,400, "Breaker Value");
-        show_image(src6, 400,400, "Breaker Value");
+            //List of strings urls
+            var urlsOfPics = ["https://live.staticflickr.com/65535/49667651786_eaf87c6e0b_b.jpg","https://live.staticflickr.com/65535/49667651751_60cf38e2ae.jpg", 
+            "https://live.staticflickr.com/65535/49667651776_44c46ac95e_b.jpg", "https://live.staticflickr.com/65535/49667935067_7dbc48bd46_b.jpg",
+            "https://live.staticflickr.com/65535/49667935052_0a0c888258_b.jpg", "https://live.staticflickr.com/65535/49667935092_cc4f2aa137_b.jpg" ];
+            //Loop through the urlOfPics array 
+            for(var i = 0; i < urlsOfPics.length; i++){
+                show_image(urlsOfPics[i], 400,400, "Breaker Value_" + i);
+            }
     }
 }
 
