6793917ef70cf858796910019b821b695c329e79
pelimimian <pelimimian7@gmail.com>
2020-05-01 15:00:42 -0400

    Refactored the code display any list of url depending on what the user clicks on and changed the html tag to include this.id as a parameter. ~ 1hr 30min

--- mainView.js
+++ mainView.js
@@ -2,25 +2,28 @@
 //Key caa57ad8874db3c27dd35f3a812b70f4
 //Secret ae4f050c5fdf3b28
 
-function assignPicture(){
-    displayPicture(document.getElementsByTagName("td"));
-}
+//List of strings urls
+var urlsOf17APics = ["17A","https://live.staticflickr.com/65535/49667651786_eaf87c6e0b_b.jpg","https://live.staticflickr.com/65535/49667651751_60cf38e2ae.jpg", 
+"https://live.staticflickr.com/65535/49667651776_44c46ac95e_b.jpg", "https://live.staticflickr.com/65535/49667935067_7dbc48bd46_b.jpg",
+"https://live.staticflickr.com/65535/49667935052_0a0c888258_b.jpg", "https://live.staticflickr.com/65535/49667935092_cc4f2aa137_b.jpg" ];
 
-function displayPicture(rowElementsByTag) {
-    //Searching throuh the list of HTML Collection elements that have td tags for find id's strings.
-    for(var i = 0; i , rowElementsByTag.length; i++){
-        var breakerValue = rowElementsByTag[i].id;
-        if (breakerValue.localeCompare("17A") == 0 ){
-            //List of strings urls
-            var urlsOfPics = ["https://live.staticflickr.com/65535/49667651786_eaf87c6e0b_b.jpg","https://live.staticflickr.com/65535/49667651751_60cf38e2ae.jpg", 
-            "https://live.staticflickr.com/65535/49667651776_44c46ac95e_b.jpg", "https://live.staticflickr.com/65535/49667935067_7dbc48bd46_b.jpg",
-            "https://live.staticflickr.com/65535/49667935052_0a0c888258_b.jpg", "https://live.staticflickr.com/65535/49667935092_cc4f2aa137_b.jpg" ];
-            //Loop through the urlOfPics array 
-            for(var i = 0; i < urlsOfPics.length; i++){
-                show_image(urlsOfPics[i], 400,400, "Breaker Value_" + i);
+var urlsOf12BPics = ["12B", "https://live.staticflickr.com/65535/49838364831_f2df50cba9_b.jpg", "https://live.staticflickr.com/65535/49838364901_34cd8f18b9_b.jpg",
+"https://live.staticflickr.com/65535/49837819848_a3351a8b9d_b.jpg", "https://live.staticflickr.com/65535/49837819873_e636a079dc_b.jpg",
+"https://live.staticflickr.com/65535/49837819888_3772c3b42c_b.jpg", "https://live.staticflickr.com/65535/49837819838_64337c8f90_b.jpg"];
+
+var listofBreakerValuePics = [urlsOf12BPics, urlsOf17APics];
+
+function displayPicture(nameOfId) {
+  //Go through the list of urls for each Ids. If the ID clicked on is equal to the name of the id in the list of urls, display all pictures
+  //From that particular list
+    for(var i = 0; listofBreakerValuePics.length; i++){
+        var a = listofBreakerValuePics[i];
+        if(a.includes(nameOfId)){
+            for(var j = 1; j < a.length; j++){//Starting at 1 because the the first element in the list is not a link to a picture.
+                show_image(a[j], 400,400, "Breaker Value_" + j);
             }
         }
-     }
+    }
 }
 
 
